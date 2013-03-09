0246095d521f99f3bbb1e08cd343b18f2fdfe308
Keith Pitt <me@keithpitt.com>
2013-03-09 16:46:01 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fundler::Application.config.secret_key_base = 'b8e26b9bba1b370b52f5188b587e9e90296e54f931b5c0357d73fe6162cc72500daed0d64e2c880e13e0769b888bf6d99870b5786a09ba2602a8f7f465a689f1'
